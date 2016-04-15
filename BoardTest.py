bd742b66de9a011c81f463e237367f2e8f71a3d8
Ryan Kingsley <kingsler@mail.gvsu.edu>
2016-04-15 13:37:59 -0400

    finishing Board class

--- BoardTest.py
+++ BoardTest.py
@@ -16,5 +16,25 @@ class BoardTest(unittest.TestCase):
         self.assertEqual(self.Board.available_spaces(), [0,1,2,3,4,5,6,7,8],
                          'incorrect available_spaces')
 
+    def test_open_space(self):
+        self.assertEqual(self.Board.open_space(3), True,
+                         'incorrect available_spaces')
+
+    def test_take_space(self):
+        self.Board.take_space(0, 0)
+        self.assertEqual(self.Board.Board.all(), np.matrix('0 -1 -1; -1 -1 -1; -1 -1 -1').all(),
+                         'incorrect default board setup')
+
+    def test_reset_game(self):
+        self.Board.reset_game()
+        self.assertEqual(self.Board.Board.all(), np.matrix('-1 -1 -1; -1 -1 -1; -1 -1 -1').all(),
+                         'incorrect default board setup')
+
+    def test_game_status(self):
+        status = self.Board.game_status()
+        self.assertEqual(-1,status)
+        # self.assertEqual(self.Board.Board.all(), np.matrix('-1 -1 -1; -1 -1 -1; -1 -1 -1').all(),
+        #                  'incorrect default board setup')
+
 if __name__ == '__main__':
     unittest.main()
